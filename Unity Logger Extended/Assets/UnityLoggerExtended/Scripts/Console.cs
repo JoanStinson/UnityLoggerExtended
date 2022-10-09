f1627c2174e1c461a8ae19b5df03bde241b82e99
Joan Ginard Mateo <jgm7.hd@gmail.com>
2022-10-09 15:41:17 +0200

    Refactored implementation

--- Unity Logger Extended/Assets/UnityLoggerExtended/Scripts/Console.cs
+++ Unity Logger Extended/Assets/UnityLoggerExtended/Scripts/Console.cs
@@ -9,6 +9,90 @@ namespace UnityEngine
         private const string warningColor = nameof(Color.yellow);
         private const string errorColor = nameof(Color.red);
 
+        [Conditional("DEBUG")]
+        public static void Log(object message)
+        {
+            Debug.Log(FormatMessage(infoColor, message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void Log(string category, object message)
+        {
+            Debug.Log(FormatMessageWithCategory(infoColor, category, message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogFormat(string format, params object[] args)
+        {
+            Debug.Log(FormatMessage(infoColor, string.Format(format, args)));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogFormat(string category, string format, params object[] args)
+        {
+            Debug.Log(FormatMessageWithCategory(infoColor, category, string.Format(format, args)));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogWarning(object message)
+        {
+            Debug.LogWarning(FormatMessage(warningColor, message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogWarning(string category, object message)
+        {
+            Debug.LogWarning(FormatMessageWithCategory(warningColor, category, message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogWarningFormat(string format, params object[] args)
+        {
+            Debug.LogWarningFormat(FormatMessage(warningColor, string.Format(format, args)));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogWarningFormat(string category, string format, params object[] args)
+        {
+            Debug.LogWarningFormat(FormatMessageWithCategory(warningColor, category, string.Format(format, args)));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogError(object message)
+        {
+            Debug.LogError(FormatMessage(errorColor, message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogError(string category, object message)
+        {
+            Debug.LogError(FormatMessageWithCategory(errorColor, category, message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogErrorFormat(string format, params object[] args)
+        {
+            Debug.LogErrorFormat(FormatMessage(errorColor, string.Format(format, args)));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogErrorFormat(string category, string format, params object[] args)
+        {
+            Debug.LogErrorFormat(FormatMessageWithCategory(errorColor, category, string.Format(format, args)));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogException(Exception exception)
+        {
+            Debug.LogError(FormatMessage(errorColor, exception.Message));
+        }
+
+        [Conditional("DEBUG")]
+        public static void LogException(string category, Exception exception)
+        {
+            Debug.LogError(FormatMessageWithCategory(errorColor, category, exception.Message));
+        }
+
         private static string FormatMessage(string color, object message)
         {
             return $"<color={color}>{message}</color>";
@@ -17,104 +101,6 @@ namespace UnityEngine
         private static string FormatMessageWithCategory(string color, string category, object message)
         {
             return $"<color={color}><b>[{category}]</b> {message}</color>";
-        }
-
-        [Conditional("DEBUG")]
-        public static void Log(object message)
-        {
-            string messageToDisplay = $"<color=white>{message}</color>";
-            Debug.Log(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void Log(string category, object message)
-        {
-            string messageToDisplay = $"<color=white><b>[{category}]</b> {message}</color>";
-            Debug.Log(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogFormat(string format, params object[] args)
-        {
-            string messageToDisplay = $"<color=white>{string.Format(format, args)}</color>";
-            Debug.Log(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogFormat(string category, string format, params object[] args)
-        {
-            string messageToDisplay = $"<color=white><b>[{category}]</b> {string.Format(format, args)}</color>";
-            Debug.Log(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogWarning(object message)
-        {
-            string messageToDisplay = $"<color=yellow>{message}</color>";
-            Debug.LogWarning(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogWarning(string category, object message)
-        {
-            string messageToDisplay = $"<color=yellow><b>[{category}]</b> {message}</color>";
-            Debug.LogWarning(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogWarningFormat(string format, params object[] args)
-        {
-            string messageToDisplay = $"<color=yellow>{string.Format(format, args)}</color>";
-            Debug.LogWarningFormat(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogWarningFormat(string category, string format, params object[] args)
-        {
-            string messageToDisplay = $"<color=yellow><b>[{category}]</b> {string.Format(format, args)}</color>";
-            Debug.LogWarningFormat(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogError(object message)
-        {
-            string messageToDisplay = $"<color=red>{message}</color>";
-            Debug.LogError(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogError(string category, object message)
-        {
-            string messageToDisplay = $"<color=red><b>[{category}]</b> {message}</color>";
-            Debug.LogError(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogErrorFormat(string format, params object[] args)
-        {
-            string messageToDisplay = $"<color=red>{string.Format(format, args)}</color>";
-            Debug.LogErrorFormat(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogErrorFormat(string category, string format, params object[] args)
-        {
-            string messageToDisplay = $"<color=red><b>[{category}]</b> {string.Format(format, args)}</color>";
-            Debug.LogErrorFormat(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogException(Exception exception)
-        {
-            string messageToDisplay = $"<color=red>{exception.Message}</color>";
-            Debug.LogError(messageToDisplay);
-        }
-
-        [Conditional("DEBUG")]
-        public static void LogException(string category, Exception exception)
-        {
-            string messageToDisplay = $"<color=red><b>[{category}]</b> {exception.Message}</color>";
-            Debug.LogError(messageToDisplay);
         }
     }
 }
